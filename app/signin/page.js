8e3167b9761159fd4bdedae4bf36e833902f8db1
Adem zerii <adem.zeri1234@gmail.com>
2023-10-20 19:46:22 +0100

    .

--- app/signin/page.js
+++ app/signin/page.js
@@ -4,11 +4,10 @@ import { getAuth, signInAnonymously } from "firebase/auth";
 import logo from "../../public/assets/full-logo.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import QrScanner from "react-qr-scanner";
+import QrReader from 'modern-react-qr-reader';
 
 function Page() {
     const router = useRouter();
-        const scannerRef = useRef(null);
        
         const handleScan = data => {
            if (data == "https://harmonix.vercel.app/") {
@@ -32,15 +31,15 @@ function Page() {
         <div className="font-mont w-full flex flex-col gap-10 justify-center mx-auto items-center " children="hide-footer">
             <h1 className="text-4xl font-bold text-white ">Scan the QR Code to Access the App</h1>
             <div>
-                <QrScanner
+                <QrReader
                 facingMode="environment"
-                ref={scannerRef}
                 onError={handleError}
                 onScan={handleScan}
                 style={{ width: '100%' }}
             />
             </div>
             <Image src={logo} height={220} width={220} alt="aa" />
+            <p>yo</p>
         </div>
     );
 }
